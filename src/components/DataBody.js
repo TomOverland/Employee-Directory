f8c162d536c677552e1b6ad8ba68ba2dceeb844c
Tom Overland <tomoverland03@gmail.com>
2020-11-19 21:49:32 -0600

    fixed spelling err

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -5,7 +5,7 @@ function DataBody({ users }) {
   function formatDate(date) {
     const dateArray = date.split("-");
     const year = dateArray[0];
-    const month = dataArray[1];
+    const month = dateArray[1];
     const dayArray = dateArray[2].split("T");
     const day = dayArray[0];
     const formattedDate = [month, day, year].join("/");
